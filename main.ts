70d266103823a5f064250eef1153f1fb7b75d490
s21110 <122764722+s21110@users.noreply.github.com>
2023-05-08 14:10:28 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+basic.forever(function () {
+    if (input.lightLevel() >= 150) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.mysterious), SoundExpressionPlayMode.UntilDone)
+    }
+})
